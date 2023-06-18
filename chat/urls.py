0ef2dfaf44ab3bea8bd7bfe63d9d3ff2419bbe0c
unknown <raiprabhat699@gmail.com>
2023-06-18 15:22:28 +0530

    Fixed Logout

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from core import views
+from . import settings
 
 urlpatterns = [
 
@@ -10,10 +11,9 @@ urlpatterns = [
     path('', include('core.urls')),
 
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-     
-    path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/login/'},
+
+    path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL),
          name='logout'),
     path('register/', views.register, name="register"),
 
 ]
-
